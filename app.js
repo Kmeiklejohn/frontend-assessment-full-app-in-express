3b76abe29b6057f555e130fab9bb66fb2f057008
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-10-04 10:41:58 -0400

    done

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require("express")
 const path = require("path")
+const morgan = require('morgan')
 
-const app = express()
+const app = express();
 const publicFolderPath = path.join(__dirname, "public")
+
+const router = express.Router();
 
 app.use(express.json())
 app.use(express.static(publicFolderPath))
+app.use(morgan('tiny'))
+
+
+app.set("port", 5000)
 
 const users = []
 
-// add POST request listener here
+app.post('/api/user',(req,res)=>{
+    const userData = req.body;
 
-app.listen(3000);
+  const usersNow = users.map(user => user.username)
+console.log(usersNow)
+   if(!usersNow.includes(userData.username)){
+       
+       
+       userData.id = randomId();
+       users.push(userData) 
+       console.log(userData)
+       console.log(users)
+       res.status(201).send(userData)
+   }else{
+       res.status(409)
+       .send({err:"username taken"})
+   }
+})
+
+app.listen(app.get("port"), () => {
+    console.log(`app listening on port 5000`)
+});
+
+const randomId = () => Math.floor(Math.random()*10000)*1
--- public/index.js
+++ public/index.js
@@ -1,2 +1,44 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const email = document.getElementById('email')
+const about = document.getElementById('about')
+
+userCreateSubmitButton.addEventListener("click",formSubmit);
+
+function formSubmit(event){
+    event.preventDefault();
+    let userData = {
+                username: username.value,
+                password: password.value,
+                email: email.value,
+                about: about.value
+    }
+    const userJson = JSON.stringify(userData)
+    console.log(userJson)
+fetchData(userJson);
+}
+
+fetchData = (userInfo) => {
+const postRequestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: userInfo
+  };
+
+fetch('http://localhost:5000/api/user', postRequestOptions)
+    .then(res =>{ 
+        if (res.status === 201){
+            alert("Success")
+        }else{
+            alert("Username is taken")
+        } 
+        return res.json()
+    })
+        .then(data =>{
+        console.log(data)
+    }).catch(error => console.log('err',error))
+    
+}
